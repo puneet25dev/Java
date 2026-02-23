687aa2b8182c500a060c4b406f9dbdcf96f9f5b4
Puneet kumar <kpuneetoff@gmail.com>
2026-02-23 21:45:47 +0530

    Table of any num

---
+++ TableOfNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class TableOfNum {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the table number");
+        int num=sc.nextInt();
+        int table=1;
+
+        for(int i=1;i<=10;i++){
+             table=num*i;
+             System.out.println(num +" * "+ i + " = " + table);
+        }
+
+        sc.close();
+    }
+}
