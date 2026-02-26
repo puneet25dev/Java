b7af96789cbbe86b833eedb8fbb32b23cc513403
Puneet kumar <kpuneetoff@gmail.com>
2026-02-26 07:26:05 +0530

    Hollow rectangle

---
+++ Pattern/HollowRectangle.java
@@ -0,0 +1,21 @@
+package Pattern;
+import java.util.*;
+public class HollowRectangle {
+    public static void main(String args[]){
+        System.out.println("Enter how many row do you want to print");
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        System.out.println("Enter no of column");
+        int column=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=column;j++){
+                if(i==1||i==row||j==1||j==column){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
