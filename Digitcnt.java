950594d857b7f4f82431d228f220fb957227351a
Puneet kumar <kpuneetoff@gmail.com>
2026-02-28 11:12:27 +0530

    Count number length

---
+++ Digitcnt.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.*;
+public class Digitcnt{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        BigInteger num=sc.nextBigInteger();
+        int count=String.valueOf(num).length();
+        
+        System.out.println(count);
+        sc.close();
+    }
+}
