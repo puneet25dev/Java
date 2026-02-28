5f80b2a4530bcb9355cd2f7cf5520d8229ac8170
Puneet kumar <kpuneetoff@gmail.com>
2026-02-28 18:48:08 +0530

    remove hardcode

--- Oops/Main.java
+++ Oops/Main.java
@@ -1,19 +1,23 @@
 package Oops;
 
 class Student {
-    String name="Punnet";
-    int age=30;
+    String name;
+    int age;
 
-    void display(){
-        System.out.println("Your age is "+ age);
-        System.out.println("Your name is "+ name);
+    Student(String name, int age) {   
+        this.name = name;
+        this.age = age;
+    }
 
+    void display() {
+        System.out.println("Your age is " + age);
+        System.out.println("Your name is " + name);
     }
 }
 
-public class Main{
-    public static void main(String args[]){
-        Student detail=new Student();
+public class Main {
+    public static void main(String[] args) {
+        Student detail = new Student("Puneet", 30);
         detail.display();
     }
 }
