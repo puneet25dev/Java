e62cee77238b5da9d7cb1467800aa4a6e5c3b195
Puneet kumar <kpuneetoff@gmail.com>
2026-03-02 18:42:36 +0530

    Creating array

---
+++ DSA/Array/DemoArr.java
@@ -0,0 +1,11 @@
+package DSA.Array;
+
+public class DemoArr {
+    public static void main(String[] args) {
+        int arr[]=new int[3];
+        arr[0] = 92;
+        arr[1] = 93;
+        arr[2] = 94;
+        System.out.println(arr[2]);
+    }
+}
