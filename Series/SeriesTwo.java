1505cc0ec4db90f62005dd0f48ed917ef7b76349
Puneet kumar <kpuneetoff@gmail.com>
2026-03-04 18:45:56 +0530

    Series que no 1

---
+++ Series/SeriesTwo.java
@@ -0,0 +1,16 @@
+package Series;
+
+public class SeriesTwo {
+    public static void main(String[] args) {
+        int num = 23;
+        System.out.print(num);
+        
+            for (int j = 5; j <= 9; j++) {
+                num = num + j;
+                System.out.print(","+num);
+
+
+            }
+        
+    }
+}
