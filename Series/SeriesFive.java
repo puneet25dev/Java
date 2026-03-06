7bbaa42849e24c461e6bea84a54c0d047c37a835
Puneet kumar <kpuneetoff@gmail.com>
2026-03-06 07:47:46 +0530

    Series Question 20

---
+++ Series/SeriesFive.java
@@ -0,0 +1,13 @@
+package Series;
+
+public class SeriesFive {
+    public static void main(String[] args) {
+        for(int i=29;i<60;i=i+9){
+            System.out.print(i);
+
+            if(i<55){
+                System.out.print(",");
+            }
+        }
+    }
+}
