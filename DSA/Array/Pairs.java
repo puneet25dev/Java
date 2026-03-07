2bb31c33a0e7e265d34555cf4fd383962a033f83
Puneet kumar <kpuneetoff@gmail.com>
2026-03-07 19:48:38 +0530

    Pairs printing in an array

---
+++ DSA/Array/Pairs.java
@@ -0,0 +1,20 @@
+package DSA.Array;
+
+public class Pairs {
+    public static void printPairs(int numbers[]) {
+
+        for(int i=0;i<numbers.length;i++){
+            int curr=numbers[i];
+            for(int j=i+1;j<numbers.length;j++){
+                System.out.print("("+curr+","+numbers[j]+")");
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6,7,8,9};
+        printPairs(numbers);
+
+    }
+}
