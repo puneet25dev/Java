b7ce5e8f1865d2fd9e96decc5cf092f697c20090
Puneet kumar <kpuneetoff@gmail.com>
2026-03-08 12:55:46 +0530

    Removed extra variables

--- DSA/Array/SubArr.java
+++ DSA/Array/SubArr.java
@@ -6,11 +6,9 @@ public class SubArr {
 
         for(int i=0;i<numbers.length;i++){
             int sum=0;
-            int start=i;
             for(int j=i;j<numbers.length;j++){
-                int end=j;
                 System.out.print("[");
-                for(int k=start;k<=end;k++){
+                for(int k=i;k<=j;k++){
                     sum+=numbers[k];
                     System.out.print(" "+numbers[k]+ " ");
                 }
