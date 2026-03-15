9bd33cffa6776700ec8da359c66e5c2ff5f2043d
Puneet kumar <kpuneetoff@gmail.com>
2026-03-15 10:27:45 +0530

    Hollow Pattern

---
+++ Practice/HollowPattern.java
@@ -0,0 +1,25 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class HollowPattern{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row no");
+        int row=sc.nextInt();
+
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=row;j++){
+
+                if(i==1||i==row||j==1||j==row){
+                    System.out.print("*" );
+
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
