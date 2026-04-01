f10cb6e5c63d28fa2baebd0eb381e3372607003d
Puneet kumar <kpuneetoff@gmail.com>
2026-04-01 22:18:13 +0530

    series question 36

---
+++ Practice/Series36.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Series36 {
+    public static void main(String[] args) {
+
+        for (int j = 1; j <= 4; j++) {
+            for (int i = 9; i >= j; i--) {
+                System.out.print(i + ",");
+            }
+        }
+
+    }
+}
